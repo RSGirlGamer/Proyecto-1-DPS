b71bec4ca4b3a48af5990c4262bccae8a5c3ea13
DeboraRivas1805 <00059922@uca.edu.sv>
2024-10-16 12:58:49 -0600

    Arreglando

--- Proyecto-1-DPS-backend/api/src/config/db.js
+++ Proyecto-1-DPS-backend/api/src/config/db.js
@@ -7,7 +7,6 @@ console.log('DB_USER:', process.env.DB_USER);
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME
 });
 
